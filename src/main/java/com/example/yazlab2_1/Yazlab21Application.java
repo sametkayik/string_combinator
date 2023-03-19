a1c64d065914592ea2cf6415b55db126475df9bf
samet <sametkayik@gmail.com>
2023-03-19 13:24:07 +0300

    Initial commit

---
+++ src/main/java/com/example/yazlab2_1/Yazlab21Application.java
@@ -0,0 +1,13 @@
+package com.example.yazlab2_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yazlab21Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Yazlab21Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yazlab2_1/Yazlab21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yazlab2_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Yazlab21ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
