b25d4b8c13308cb9c633975ec8b1cb3bdea1fae2
Doganur <doganuraydeniz@hotmail.com>
2023-03-26 00:03:17 +0300

    algo son

--- src/main/java/com/example/yazlab2_1/services/MergeTextService.java
+++ src/main/java/com/example/yazlab2_1/services/MergeTextService.java
@@ -1,6 +1,7 @@
 package com.example.yazlab2_1.services;
 
 public class MergeTextService {
+
     public static String mergeSentences(String[] sentences) {
 
         StringBuilder result = new StringBuilder(sentences[0].toLowerCase());
@@ -9,23 +10,22 @@ public class MergeTextService {
         for (int i = 1; i < sentences.length; i++) {
             String current = sentences[i].toLowerCase();
             String[] lastWords = lastMerged.split(" ");
-            boolean foundMatch = false;
-            for (String lastWord : lastWords) {
-                if (current.startsWith(lastWord)) {
-                    foundMatch = true;
-                    break;
-                }
-            }
+            String lastWord = lastWords[lastWords.length - 1];
 
-            if (foundMatch) {
-                int index = current.indexOf(lastWords[lastWords.length-1]);
+            if (current.startsWith(lastWord)) {
+                result.append(current.substring(lastWord.length()));
+                lastMerged = current;
+            }
+            else {
+                int index = current.indexOf(lastWord);
                 if (index >= 0) {
-                    result.append(current.substring(index + lastWords[lastWords.length-1].length()));
-                    lastMerged = current;
+                    result.append(current.substring(index + lastWord.length()));
+                    lastMerged = current.substring(index);
                 }
             }
         }
-
         return result.toString();
     }
+
+
 }
