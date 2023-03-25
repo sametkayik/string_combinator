06baa2568df74d56d0585d16ddfff2a5a2f89ca1
samet <sametkayik@gmail.com>
2023-03-25 18:36:59 +0300

    Create new endpoint for nonlinear merging

--- src/main/java/com/example/yazlab2_1/controllers/TextController.java
+++ src/main/java/com/example/yazlab2_1/controllers/TextController.java
@@ -5,7 +5,9 @@ import com.example.yazlab2_1.models.TextsRequest;
 import com.example.yazlab2_1.services.TextService;
 import com.example.yazlab2_1.services.MergeTextService;
 import org.springframework.web.bind.annotation.*;
+
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 import java.util.List;
 
 @CrossOrigin
@@ -19,14 +21,13 @@ public class TextController {
     }
     @PostMapping("/mergeText")
     public TextEntity mergeText(@RequestBody TextsRequest textsRequest) {
+        long startTime = System.nanoTime();
         List<String> texts = textsRequest.getTexts();
 
-        long startTime = System.nanoTime();
         String mergedText = MergeTextService.mergeSentences(texts.toArray(new String[0]));
         long endTime = System.nanoTime();
-        long durationTimeInNanoSec = (endTime - startTime);
-        double durationTimeInSeconds = (double)durationTimeInNanoSec / 1_000_000_000.0;
-        System.out.println("Merging Time in nanoseconds: " + durationTimeInNanoSec);
+
+        double durationTimeInSeconds = (double) (endTime - startTime) / 1_000_000_000.0;
         System.out.println("Merging Time : " + new DecimalFormat("#.############").format(durationTimeInSeconds) + " seconds");
 
         TextEntity textEntity = new TextEntity();
@@ -36,16 +37,35 @@ public class TextController {
 
         return textEntity;
     }
+    @PostMapping("/mergeTextNonlinear")
+    public TextEntity mergeTextNonlinear(@RequestBody TextsRequest textsRequest) {
+
+        List<String> texts = textsRequest.getTexts();
+        List<TextEntity> textEntities = new ArrayList<>();
+
+        List<Integer> indexes = new ArrayList<>();
+        for (int i = 0; i < texts.size(); i++) {
+            indexes.add(i);
+        }
+        long startTime = System.nanoTime();
+        MergeTextService.permute(indexes, 0, textEntities, texts.toArray(new String[0]));
+        long endTime = System.nanoTime();
+        TextEntity longestTextEntity = MergeTextService.findLongestText(textEntities);
+
+        double durationTimeInSeconds = (double) (endTime - startTime) / 1_000_000_000.0;
+        System.out.println("Whole process time : " + new DecimalFormat("#.############").format(durationTimeInSeconds) + " seconds");
+
+        longestTextEntity.setDurationTime(durationTimeInSeconds);
+        return longestTextEntity;
+    }
 
     @PostMapping("/saveText")
     public void saveText(@RequestBody TextsRequest textsRequest){
         textService.saveEntity(textsRequest.getTexts(), textsRequest.getMergedText(), textsRequest.getDurationTime());
     }
+
     @GetMapping("/texts")
     public List<TextEntity> getAllEntities() {
         return textService.getAllEntities();
     }
 }
-
-
-
--- src/main/java/com/example/yazlab2_1/services/MergeTextService.java
+++ src/main/java/com/example/yazlab2_1/services/MergeTextService.java
@@ -1,5 +1,11 @@
 package com.example.yazlab2_1.services;
 
+import com.example.yazlab2_1.models.TextEntity;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
+
 public class MergeTextService {
     public static String mergeSentences(String[] sentences) {
 
@@ -27,5 +33,55 @@ public class MergeTextService {
         }
 
         return result.toString();
+    }
+    public static void permute(List<Integer> indexes, int start, List<TextEntity> textEntities, String[] texts) {
+        long startTime = System.nanoTime();
+        if (start == indexes.size()) {
+            String[] permutedTexts = new String[texts.length];
+            for (int i = 0; i < indexes.size(); i++) {
+                permutedTexts[i] = texts[indexes.get(i)];
+            }
+
+            String mergedText = MergeTextService.mergeSentences(permutedTexts);
+            long endTime = System.nanoTime();
+            double durationTimeInSeconds = (double) (endTime - startTime) / 1_000_000_000.0;
+
+            System.out.println("Merged Text: " + mergedText);
+            System.out.println("Merging Time : " + new DecimalFormat("#.############").format(durationTimeInSeconds) + " seconds");
+
+            TextEntity textEntity = new TextEntity();
+            textEntity.setTexts(Arrays.asList(permutedTexts));
+            textEntity.setMergedText(mergedText);
+            textEntity.setDurationTime(durationTimeInSeconds);
+            textEntities.add(textEntity);
+
+        } else {
+            for (int i = start; i < indexes.size(); i++) {
+                int temp = indexes.get(start);
+                indexes.set(start, indexes.get(i));
+                indexes.set(i, temp);
+
+                permute(indexes, start + 1, textEntities, texts);
+
+                temp = indexes.get(start);
+                indexes.set(start, indexes.get(i));
+                indexes.set(i, temp);
+            }
+        }
+    }
+    public static TextEntity findLongestText(List<TextEntity> textEntities) {
+        TextEntity longestTextEntity = null;
+        int maxLength = 0;
+
+        for (TextEntity textEntity : textEntities) {
+            String mergedText = textEntity.getMergedText();
+            int length = mergedText.length();
+            if (length > maxLength) {
+                maxLength = length;
+                longestTextEntity = textEntity;
+            }
+        }
+
+        return longestTextEntity;
     }
 }
